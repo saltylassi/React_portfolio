a486fdf3b1bb934818a81978dfe8f9f3d85643da
saltylassi <illke@naver.com>
2021-03-08 11:01:00 +0900

    feat:Add Router

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,8 @@
 const HOME = "/";
+const ABOUT = "/about";
+
+const PROJECTS = "/projects";
+const PROJECT_DETAIL = "/:id";
 
 const routes = {
     home: HOME,
@@ -6,6 +10,11 @@ const routes = {
     // deleteVideo: (id) => {
     //     return id ? `/videos/${id}/delete` : DELETE_VIDEO;
     // },
+    about: ABOUT,
+    projects: PROJECTS,
+    projectDetail: (id) => {
+        return id ? `/project/${id}` : PROJECT_DETAIL;
+    },
 };
 
 export default routes;
